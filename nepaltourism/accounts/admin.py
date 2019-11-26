49fc73afa02c5c88c1de2233a65efd0678dc4515
bibekebib <42284727+bibekebib@users.noreply.github.com>
2019-11-26 19:42:05 +0545

    merging

--- nepaltourism/accounts/admin.py
+++ nepaltourism/accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import *
+from django.contrib.auth.models import *
 # Register your models here.
+admin.site.site_header = 'Nepal Tourism Board'
+@admin.register(profile)
+class profileadmiin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','email','phone','address')
+    list_display_links  = ('email',)
+    list_editable = ('first_name', 'last_name')
+    list_filter = ('email',)
+    search_fields = ('first_name',)
+admin.site.unregister(Group)
+admin.site.register(font)
--- nepaltourism/accounts/models.py
+++ nepaltourism/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class profile (models.Model):
+    first_name = models.CharField(max_length=100, blank=False)
+    last_name = models.CharField(max_length=100, blank=False)
+    email = models.CharField(max_length = 100, blank = False)
+    phone = models.CharField(max_length = 15, blank = False)
+    address = models.CharField(max_length = 50, blank=False)
+
+    def __str__(self):
+        return self.email
+
+class font(models.Model):
+    font_size = models.IntegerField()
